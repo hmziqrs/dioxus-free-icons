1281adb7a1be9cee67f3eace6442760410447071
hmziqrs <hmziqrs@gmail.com>
2025-04-08 20:58:20 +0500

    example and lib name fix

--- packages/example/src/main.rs
+++ packages/example/src/main.rs
@@ -1,7 +1,7 @@
 use dioxus::prelude::*;
 
-use dioxus_free_icons::icons::fa_brands_icons::FaRust;
-use dioxus_free_icons::Icon;
+use hmziq_dioxus_free_icons::icons::fa_brands_icons::FaRust;
+use hmziq_dioxus_free_icons::Icon;
 
 fn main() {
     // init debug tool for WebAssembly
@@ -13,16 +13,13 @@ fn main() {
 
 fn app() -> Element {
     rsx! (
-        div {
-            style: "text-align: center;",
+        div { style: "text-align: center;",
             h1 { "🌗 Dioxus 🚀" }
             h3 { "Frontend that scales." }
-            p { "Dioxus is a portable, performant, and ergonomic framework for building cross-platform user interfaces in Rust." },
-            Icon {
-                width: 60,
-                height: 60,
-                icon: FaRust,
+            p {
+                "Dioxus is a portable, performant, and ergonomic framework for building cross-platform user interfaces in Rust."
             }
+            Icon { width: 60, height: 60, icon: FaRust }
         }
     )
 }
